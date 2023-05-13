bb6e805b822b0dca4880f8b602bd893c475eb89c
SOUROV <sourovsarker005@gmail.com>
2023-05-13 16:20:56 +0600

    Initial commit But 3:22:05

---
+++ base/views.py
@@ -0,0 +1,145 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from .models import Room, Topic, Message
+from .forms import RoomForm
+
+
+# Create your views here.
+
+# rooms = [
+#     {'id': 1, 'name': 'Web Developer'},
+#     {'id': 2, 'name': 'Design Segment'},
+#     {'id': 3, 'name': 'Frontend Design'},
+# ]
+
+def loginPage(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username, password=password)
+
+        except:
+            messages.error(request, 'User doses not exist.')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username OR Password does not exist.')
+
+    context = {'page': page}
+    return render(request, 'chatTWS/login_register.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
+
+def registerPage(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occurred during registration!!')
+
+    return render(request, 'chatTWS/login_register.html', {'form': form})
+
+
+def home(request):
+    q = request.GET.get('q') if request.GET.get('q') is not None else ''
+
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
+
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
+    return render(request, 'chatTWS/home.html', context)
+
+
+def room(request, pk):
+    roomNum = Room.objects.get(id=pk)
+    room_messages = roomNum.message_set.all().order_by('-created')
+    participants = roomNum.participants.all()
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=roomNum,
+            body=request.POST.get('body')
+        )
+        return redirect('room', pk=roomNum.id)
+
+    context = {'room': roomNum, 'room_messages': room_messages, 'participants': participants}
+    return render(request, 'chatTWS/room.html', context)
+
+
+@login_required(login_url='login')
+def createRoom(request):
+    form = RoomForm()
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'chatTWS/room_form.html', context)
+
+
+@login_required(login_url='login')
+def updateRoom(request, pk):
+    rooms = Room.objects.get(id=pk)
+    form = RoomForm(instance=rooms)
+
+    if request.user != rooms.host:
+        return HttpResponse('You are not allowed here!!')
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=rooms)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'chatTWS/room_form.html', context)
+
+
+@login_required(login_url='login')
+def deleteRoom(request, pk):
+    rooms = Room.objects.get(id=pk)
+
+    if request.user != rooms.host:
+        return HttpResponse('You are not allowed here!!')
+
+    if request.method == 'POST':
+        rooms.delete()
+        return redirect('home')
+    return render(request, 'chatTWS/delete.html', {'obj': rooms})
