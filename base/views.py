1b99f12797501c1c54a02e84b913fae64d985383
SOUROV <sourovsarker005@gmail.com>
2023-06-23 03:38:23 +0600

    Mobile Responsiveness 05:53:00

--- base/views.py
+++ base/views.py
@@ -75,7 +75,7 @@ def home(request):
         Q(description__icontains=q)
     )
 
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
@@ -191,3 +191,14 @@ def updateUser(request):
             return redirect('user-profile', pk=user.id)
 
     return render(request, 'chatTWS/update-user.html', {'form': form})
+
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') is not None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, 'chatTWS/topics.html', {'topics': topics})
+
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    return render(request, 'chatTWS/activity.html', {'room_messages': room_messages})
